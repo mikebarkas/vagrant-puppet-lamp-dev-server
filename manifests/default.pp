2184e7fa6a56eb74dff9cec9873d4a65cdc648b3
Mike Barkas <mike@barkas.com>
2016-01-16 22:42:48 -0500

    Start manifest list.

---
+++ manifests/default.pp
@@ -0,0 +1,10 @@
+Exec { path => [ "/bin/", "/sbin/", "/usr/bin/", "/usr/sbin" ] }
+
+#
+# Include Modules.
+#
+include system_update
+include apache
+#include php
+#include mariadb
+#include composer
