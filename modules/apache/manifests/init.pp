84123c48853a055c7916f665054cd7d33e498e40
Mike Barkas <mike@barkas.com>
2016-01-17 20:52:04 -0500

    Enabling rewrite module for Apache.

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,18 +1,25 @@
 class apache {
   
-  # Apache packages
+  # Apache package.
   package { 'apache2' :
     ensure => present,
     require => Class['system_update'];
   }
 
-  # Apache service
+  # Start Apache service.
   service { 'apache2' :
     ensure => 'running',
     require => Package['apache2'];
   }
 
-  # Apache config
+  # Enable rewrite module.
+  file { "/etc/apache2/mods-enabled/rewrite.load":
+    ensure => link,
+    target => "/etc/apache2/mods-available/rewrite.load",
+    require => Package["apache2"]
+  }
+
+  # Placeholder index file.
   file { '/var/www/index.html' :
     ensure => directory,
     owner => 'root',
