47d1bc7d3ad192bb86a0e63f490aebd2c0ba9640
Mike Barkas <mike@barkas.com>
2016-01-17 21:02:17 -0500

    Create and enable vhost for Vagrant application.

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -13,10 +13,35 @@ class apache {
   }
 
   # Enable rewrite module.
-  file { "/etc/apache2/mods-enabled/rewrite.load":
+  file { '/etc/apache2/mods-enabled/rewrite.load':
     ensure => link,
-    target => "/etc/apache2/mods-available/rewrite.load",
-    require => Package["apache2"]
+    target => '/etc/apache2/mods-available/rewrite.load',
+    require => Package['apache2']
+  }
+
+  # Ensure directory exists for vhost.
+  file {'/etc/apache2/sites-enabled':
+    ensure => directory,
+    recurse => true,
+    purge => true,
+    force => true,
+    before => File['/etc/apache2/sites-enabled/vagrant_app'],
+    require => Package['apache2'],
+  }
+
+  # Create Vagrant vhost file.
+  file { '/etc/apache2/sites-available/vagrant_app':
+    ensure => present,
+    source => 'puppet://modules/apache/vagrant_app',
+    require => Package['apache2'],
+  }
+
+  # Enable the Vagrant vhost.
+  file { '/etc/apache2/sites-enabled/vagrant_app':
+    ensure => link,
+    target => '/etc/apache2/sites-available/vagrant_app',
+    require => File['/etc/apache2/sites-available/vagrant_app'],
+    notify => Service['apache2'],
   }
 
   # Placeholder index file.
