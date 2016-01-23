5e8cd3b1812ab82852cd2047f86dbe3e6b94f7a7
Mike Barkas <mike@barkas.com>
2016-01-22 21:59:35 -0500

    Include php xdebug and custom php settings.

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -2,16 +2,26 @@ class php {
 
   # PHP packages
   package {['php5',
-            'libapache2-mod-php5',
-            'php5-gd',
-            'php5-cli',
-            'php5-curl']:
+    'libapache2-mod-php5',
+    'php5-gd',
+    'php5-cli',
+    'php5-curl',
+    'php5-xdebug',
+  ]:
 
     ensure => present,
     require => Class['system_update'],
   }
 
-
   # PHP config
-  
+  file { 'vagrant-php-ini':
+    path => '/etc/php5/apache2/conf.d/vagrant-php.ini',
+    ensure => present,
+    owner => 'root',
+    group => 'root',
+    mode => '0777',
+    source => 'puppet:///modules/php/vagrant-php.ini',
+    require => Package['php5'],
+    notify => Service['apache2'],
+  }
 }
