a32ad67aa920c3debbde9d107bc6c7129feb3c89
Mike Barkas <mike@barkas.com>
2016-01-23 17:50:22 -0500

    Adding database for a Drupal install.

--- puppet/modules/mysql/manifests/init.pp
+++ puppet/modules/mysql/manifests/init.pp
@@ -2,13 +2,13 @@ class mysql {
   
   # MySQL packages
   package {['mysql-server',
-            'libapache2-mod-auth-mysql',
-            'php5-mysql']:
+    'libapache2-mod-auth-mysql',
+    'php5-mysql'
+    ]:
 
     ensure => present,
     require => Class['system_update'],
   }
-
 
   # MySQL service
   service { 'mysql' :
@@ -18,9 +18,16 @@ class mysql {
 
   # MySQL root password.
   exec { 'set mysql password':
-    unless  => 'mysqladmin -uroot -proot status',
-    command => "mysqladmin -uroot password vagrant",
+    unless  => 'mysqladmin -uroot -pvagrant status',
+    command => 'mysqladmin -uroot password vagrant',
     path    => ['/bin', '/usr/bin'],
     require => Service['mysql'];
   }  
+
+  # Create a database for Drupal.
+  exec { 'create database':
+    unless => '/usr/bin/mysql -uvagrant -pvagrant drupal',
+    command => '/usr/bin/mysql -uroot -pvagrant -e \'create database drupal; grant all on drupal.* to vagrant@localhost identified by "vagrant";\'',
+    require => Service['mysql'],
+  }
 }
