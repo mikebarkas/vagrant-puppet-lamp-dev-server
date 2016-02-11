9e6765e50a8058f1d9689963aa62fa1cf2501afd
Mike Barkas <mike@barkas.com>
2016-02-11 14:34:56 -0500

    Setting Composer home directory.

--- puppet/modules/composer/manifests/init.pp
+++ puppet/modules/composer/manifests/init.pp
@@ -2,7 +2,7 @@ class composer() {
 
   exec { 'install composer' :
     command => 'curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer',
-    environment => 'COMPOSER_HOME=/home/vagrant/',
+    environment => 'COMPOSER_HOME=/home/vagrant/.composer',
     require => Class['Curl'],
   }
 
